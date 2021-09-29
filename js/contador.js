afa6dc4dc1e352d95fb1ba604fdf3a149d46c188
Anahí Foresi <anahiforesi@gmail.com>
2021-09-28 21:53:57 -0300

    Favicon. Limpieza. Modif nosotros img-texto propio

--- js/contador.js
+++ js/contador.js
@@ -1,32 +1,29 @@
 //Elementos HTML
-var span = document.querySelectorAll(".num");
-console.log(span);
+let span = document.querySelectorAll(".num");
 
 //Fecha de estreno
-var fechaEstreno = new Date('12/15/2021 8:00 PM');
-console.log(fechaEstreno);
+let fechaEstreno = new Date('12/15/2021 8:00 PM');
 
 //Manejo de dias, horas, minutos y segundos
-var segundos = 1000;
-var minutos = segundos * 60;
-var horas = minutos * 60;
-var dias = horas * 24;
+let segundos = 1000;
+let minutos = segundos * 60;
+let horas = minutos * 60;
+let dias = horas * 24;
 
 function cuentaRegresiva(){
     //Calculos
-    var dyhActual = new Date();
-    var duracion = fechaEstreno - dyhActual;
-    var diasRestantes = Math.floor(duracion / dias);
-    var horasRestantes = Math.floor((duracion % dias) / horas);
-    var minRestantes = Math.floor((duracion % horas) / minutos);
-    var segRestantes = Math.floor((duracion % minutos) / segundos);
+    let dyhActual = new Date();
+    let duracion = fechaEstreno - dyhActual;
+    let diasRestantes = Math.floor(duracion / dias);
+    let horasRestantes = Math.floor((duracion % dias) / horas);
+    let minRestantes = Math.floor((duracion % horas) / minutos);
+    let segRestantes = Math.floor((duracion % minutos) / segundos);
 
     //Imprimir en HTML
     span[0].innerHTML = diasRestantes;
     span[1].innerHTML = horasRestantes;
     span[2].innerHTML = minRestantes;
-    span[3].innerHTML =segRestantes;
-    /* console.log('Faltan: ' + diasRestantes + 'días ' + horasRestantes + 'hs ' + minRestantes + 'min ' + segRestantes + 'seg'); */
+    span[3].innerHTML = segRestantes;
 }
 
 //Iniciar contador
